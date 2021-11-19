1dd289fce3b6d39715ce7b7c0a01f753f943e41d
seoungho99 <c99@kakao.com>
2021-11-19 17:30:12 +0900

    dev StartPage

--- web/src/components/Start.js
+++ web/src/components/Start.js
@@ -1,42 +1,45 @@
-import React, {useState} from 'react'
+import React from 'react'
+import { useHistory } from 'react-router-dom'
 
-const Start = () => {
-    let [이름, 이름변경] = useState('')
-    let [성별, 성별변경] = useState('')
-    
-
+const Start = (props) => {
+    let history = useHistory();
     return (
         <div>
             <h1>직업심리검사</h1>
             <p><input
-                onChange={(e) => { 이름변경(e.target.value) }}
+                onChange={props.onName}
                 type="text"
                 name="name"
                 placeholder="이름"
             /></p>
             <input
-                onClick={(e) => { 성별변경("남자") }}
-                class="form-check-input"
+                onClick={() => { props.onGender("남자") }}
+                className="form-check-input"
                 type="radio"
                 name="flexRadioDefault"
                 id="flexRadioDefault1" />
             <label
-                class="form-check-label"
-                for="flexRadioDefault1">
+                className="form-check-label"
+                htmlFor="flexRadioDefault1">
                 남자
             </label>
             <input
-                onClick={(e) => { 성별변경("여자") }}
-                class="form-check-input"
+                onClick={() => { props.onGender("여자") }}
+                className="form-check-input"
                 type="radio"
                 name="flexRadioDefault"
                 id="flexRadioDefault1" />
             <label
-                class="form-check-label"
-                for="flexRadioDefault1">
+                className="form-check-label"
+                htmlFor="flexRadioDefault1">
                 여자
             </label>
-            <p><button type="button" class="btn btn-primary">시작하기</button></p>
+            <p><button
+                type="button"
+                className="btn btn-primary"
+                disabled={!props.name || !props.gender}
+                onClick={() => { history.push('/ex') }}
+            >시작하기</button></p>
         </div>
     )
 }
