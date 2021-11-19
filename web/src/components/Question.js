d6c44b70187ba27772f4db7ae9c2b10e80a35a8b
seoungho99 <c99@kakao.com>
2021-11-19 17:31:06 +0900

    dev Question

--- web/src/components/Question.js
+++ web/src/components/Question.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 const Question = () => {
+    let history = useHistory();
     return (
         <div>
-            question
+            <h1>검사진행</h1>
+            <div class="progress">
+                <div class="progress-bar" role="progressbar"
+                    style={{ width: "25%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <Problems />
+            <p>
+                <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => { history.push('/question') }}>이전</button>
+                <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => { history.goBack() }}
+                >다음</button>
+            </p>
+        </div>
+    )
+}
+
+const Problems = () => {
+    return (
+        <div class="card text-center">
+            <div class="card-header">
+                1번
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">첫번째문항</h5>
+                <p class="card-text">
+                    <button></button>
+                </p>
+            </div>
         </div>
     )
 }
