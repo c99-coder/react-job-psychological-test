1429c66b51d749172ca49c7c2188c3a196d288b8
seoungho99 <c99@kakao.com>
2021-11-19 17:30:54 +0900

    dev QuestionEx

---
+++ web/src/components/QuestionEx.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+const QuestionEX = () => {
+    let history = useHistory();
+    return (
+        <div>
+            <h1>검사예시<span>25%</span></h1>
+            
+            <div class="progress">
+                <div class="progress-bar" role="progressbar"
+                    style={{width: "25%"}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <p>직업과 관련된 두개의 가치 중에서 자기에게 더 중요한 가치에 표시하세요.</p>
+            <p>가치의 뜻을 잘 모르겠다면 문항 아래에 있는 가치의 설명을 확인해보세요.</p>
+            <p><button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => { history.push('/question') }}
+            >시작하기</button></p>
+        </div>
+        
+    )
+}
+
+export default QuestionEX
